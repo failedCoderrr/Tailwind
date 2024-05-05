b93bf1532b8c0bdf9d9953681968cc7dd33b0964
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-05-05 22:17:51 +0530

    ..

--- 02_Project/tailwind.config.js
+++ 02_Project/tailwind.config.js
@@ -3,11 +3,12 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      screens:{
-        'media560':'560px',
-        'pankaj': '1200px',
-      // => @media (min-width: 1024px) { ... }
-      }
+      screens: {
+        media440: "440px",
+        media560: "560px",
+        pankaj: "1200px",
+        // => @media (min-width: 1024px) { ... }
+      },
     },
   },
   plugins: [],
