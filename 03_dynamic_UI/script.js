84e816f061282e581ed93182889017ef538919a4
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-05-07 12:33:41 +0530

    script.js

--- 03_dynamic_UI/script.js
+++ 03_dynamic_UI/script.js
@@ -1,5 +1,7 @@
 const menuButton = document.querySelector("#button");
+const header = document.querySelector("header");
 
 menuButton.addEventListener("click", () => {
   menuButton.classList.toggle("active");
+  // header.classList.toggle('open-menu');
 });
