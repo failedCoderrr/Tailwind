9b5436fc8b71f53c67130b6c391c5d728496dde9
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-05-08 11:31:24 +0530

    carousel

--- 04_carrousel/script.js
+++ 04_carrousel/script.js
@@ -11,3 +11,22 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var swiper = new Swiper(".mySwiper3", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
